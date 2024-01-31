57834656cedd51d45c4a3326d550178b01e0ae5f
soyoong <huuhau.hh47@gmail.com>
2024-01-31 07:37:13 +0700

    Update source 2

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,46 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import { responseEnhancer } from '~/middleware/express-formatter/index'
 import dbConnection from './api/models'
+import AppRoutes from './api/routes'
 import appConfig from './config/app.config'
 
-const app: Application = express()
+export default class App {
+  constructor() {}
 
-const corsOptions: CorsOptions = {
-  origin: '*'
+  public initialize(app: Application) {
+    this.config(app)
+    this.syncDatabase()
+    // New app routes
+    new AppRoutes(app)
+  }
+
+  private config(app: Application) {
+    const corsOptions: CorsOptions = {
+      origin: '*'
+    }
+    // Accept json body request
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    // (helmet) helps secure Express apps by setting HTTP response headers.
+    app.use(helmet())
+    app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+    // (morgan) HTTP request logger middleware for node.js
+    app.use(morgan('dev'))
+    // (cors) Provide some options Headers for accept others localhost to allow request
+    app.use(cors(corsOptions))
+    // Handle custom formatter response express (middleware)
+    app.use(responseEnhancer())
+  }
+
+  private syncDatabase() {
+    dbConnection.sequelize?.sync()
+  }
 }
-// Accept json body request
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-// (helmet) helps secure Express apps by setting HTTP response headers.
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
-// (morgan) HTTP request logger middleware for node.js
-app.use(morgan('dev'))
-// (cors) Provide some options Headers for accept others localhost to allow request
-app.use(cors(corsOptions))
-// Handle custom formatter response express (middleware)
-app.use(responseEnhancer())
 
-// sync database
-dbConnection.sequelize?.sync()
+const app: Application = express()
+const appServer: App = new App()
+
+appServer.initialize(app)
 
 app
   .listen(appConfig.port, 'localhost', function () {
